6587fed084912de2fb7454ce998e84b8b9a6430b
kenmoh <kenneth.aremoh@gmail.com>
2025-05-20 08:39:09 +0100

    fix uuid

--- app/utils/utils.py
+++ app/utils/utils.py
@@ -56,7 +56,7 @@ async def get_payment_link(id: UUID, amount: Decimal, current_user: User):
     try:
         headers = {"Authorization": f"Bearer {settings.FLW_SECRET_KEY}"}
         details = {
-            "tx_ref": id,
+            "tx_ref": str(id),
             "amount": str(amount),
             "currency": "NGN",
             "redirect_url": f"{servipal_base_url}/payment/order-payment-callback",
