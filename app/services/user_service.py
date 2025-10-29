346edd0fe7c0abac98bc45ca56ce892f54105fd6
kenmoh <kenneth.aremoh@gmail.com>
2025-10-29 02:15:48 +0100

    change isnot to is_not

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -156,7 +156,6 @@ async def get_riders(db: AsyncSession, lat: float, lng: float) -> List[RiderProf
         .outerjoin(Profile.profile_image)   
         .outerjoin(Delivery, Delivery.rider_id==User.id)
         .outerjoin(Review, Review.reviewee_id == User.id)
-        .options(joinedload(User.profile))
         .where(func.ST_DWithin(User.location_coordinates, point, 100_000))
         .where(User.user_type == UserType.RIDER, User.has_delivery.is_(False), User.is_online.is_(True), Profile.profile_image != None)
         .group_by(User.id, Profile.user_id)
